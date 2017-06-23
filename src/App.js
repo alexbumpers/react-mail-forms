9bb4ba50ed069215be281234b3d921444303db1f
Alex Bumpers <apbumpers@gmail.com>
2017-06-22 21:40:39 -0500

    Render react-email-form, no event on send.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import { Form } from 'antd';
+import { Form, Input, Button } from 'antd';
 import './App.css';
 import 'antd/dist/antd.css';
 const FormItem = Form.Item;
-// const formItemLayout = {
-//   labelCol: { span: 4 },
-//   wrapperCol: { span: 8 },
-// };
-// const formTailLayout = {
-//   labelCol: { span: 4 },
-//   wrapperCol: { span: 8, offset: 4 },
-// };
-
+const addressSize = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 5 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 12 },
+  },
+};
+const bodySize = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 5 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 12 },
+  },
+};
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Form type="textarea">
-            <FormItem rows={4} label="Email address"></FormItem>
+        <h1> Quick, simple emails with React and Node.</h1>
+        <br/>
+        <Form>
+          <FormItem
+            {...addressSize}
+            label="     "
+          >
+          <Input placeholder="Email address" />
+          </FormItem>
+          <FormItem
+            {...bodySize}
+            label="     " color="white"
+          >
+          <Input placeholder="Your mesage here..." type="textarea" rows={8} id="success" />
+          <br/>
+          </FormItem>
+          <div className="EmailButton"><Button type="primary"> Send </Button></div>
         </Form>
+
       </div>
+
     );
   }
 }
